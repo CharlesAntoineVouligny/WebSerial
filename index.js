8d3980d70f470b5e86decc78fc1c5622795a1ae4
LIZARDWIZARD69 <cavouligny@gmail.com>
2022-03-12 00:23:55 -0500

    read data

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ async function connect() {
   
   // Wait for the serial port to open.
   await port.open({ baudRate: 9600 });
+
+
+  // Listen to data coming from the serial device.
+  const textDecoder = new TextDecoderStream();
+  const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+  const reader = textDecoder.readable.getReader();
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done) {
+      // Allow the serial port to be closed later.
+      reader.releaseLock();
+      break;
+    }
+    // value is a string will be streaming here.
+    console.log(value);
+  }
 }
-
-
-
