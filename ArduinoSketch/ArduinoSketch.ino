d7bd5c8f25dc89aaa25c3f2309fb2bb6d1fa9f6e
LIZARDWIZARD69 <cavouligny@gmail.com>
2022-03-12 22:02:09 -0500

    MajorChanges

--- ArduinoSketch/ArduinoSketch.ino
+++ ArduinoSketch/ArduinoSketch.ino
@@ -1,8 +1,13 @@
+// Non-interrupting delay
+unsigned long previousMillis = 0;
+
+const long interval = 1000;
+
 void setup() {
   // initialize both serial ports:
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-  
+ 
 }
 
 void loop() {
@@ -10,13 +15,25 @@ void loop() {
   if (Serial.available()) {
     char incoming = Serial.read();
     switch(incoming) {
-      case 'A' : 
+      case 'H' : 
         digitalWrite(13, HIGH);
         break;
-       case 'B' :
+       case 'L' :
         digitalWrite(13, LOW);
         break;
     }
+  } else {
+      // Print a little bit of data for devellopment purposes
+      unsigned long currentMillis = millis();
+
+      if (currentMillis - previousMillis >= interval) {
+        // save the last time you sent data
+        previousMillis = currentMillis;
+        Serial.print(F("Hello World!\r\n"));
+        
+  }
+
+        
   }
 
  
--- index.js
+++ index.js
@@ -1,30 +1,84 @@
-https://fidisys.com/blog/serial-port-devices/
+// https://fidisys.com/blog/serial-port-devices/
+
+// https://github.com/mmiscool/serialTerminal.com
 
 if ("serial" in navigator) {
-  console.log("Awesome, The serial port is supported.")
-  // The Web Serial API is supported.
+  console.log("Awesome, the serial port is supported.")
 }
 
-async function connect() {
-  // Prompts user for device
-  const port = await navigator.serial.requestPort();
-  
-  // Wait for the serial port to open.
-  await port.open({ baudRate: 9600 });
+var port, textEncoder, writableStreamClosed, writer;
 
+  async function connectSerial() {
+      try {
+          // Prompt user to select any serial port.
+          port = await navigator.serial.requestPort();
+          await port.open({ baudRate: document.getElementById("cmdLine").value });
+          listenToPort();
 
-  // Listen to data coming from the serial device.
-  const textDecoder = new TextDecoderStream();
-  const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
-  const reader = textDecoder.readable.getReader();
-  while (true) {
-    const { value, done } = await reader.read();
-    if (done) {
-      // Allow the serial port to be closed later.
-      reader.releaseLock();
-      break;
-    }
-    // value is a string will be streaming here.
-    console.log(value);
+          textEncoder = new TextEncoderStream();
+          writableStreamClosed = textEncoder.readable.pipeTo(port.writable);
+
+          writer = textEncoder.writable.getWriter();
+      } catch {
+          alert("Serial Connection Failed");
+      }
+
+      connected();
   }
-}
+
+  function connected() {
+    //Update UI
+    document.getElementById("cmdLine").value = "";
+    document.getElementById("cmdLine").placeholder = "Send data";
+    document.getElementById("cmdButton").onclick = sendSerialLine;
+    document.getElementById("cmdButton").innerHTML = "Send";
+
+  }
+ 
+  async function sendSerialLine() {
+      dataToSend = document.getElementById("cmdLine").value;
+      // if (document.getElementById("addLine").checked == true) dataToSend = dataToSend + "\r\n";
+      // if (document.getElementById("echoOn").checked == true) appendToTerminal("> " + dataToSend);
+      await writer.write(dataToSend);
+      document.getElementById("cmdLine").value = "";
+      //await writer.releaseLock();
+  }
+
+  async function listenToPort() {
+      const textDecoder = new TextDecoderStream();
+      const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+      const reader = textDecoder.readable.getReader();
+
+      // Listen to data coming from the serial device.
+      while (true) {
+          const { value, done } = await reader.read();
+          if (done) {
+              // Allow the serial port to be closed later.
+              //reader.releaseLock();
+              break;
+          }
+          // value is a string.
+          appendToTerminal(value);
+      }
+  }
+
+  const serialResultsDiv = document.getElementById("serialResults");
+
+  async function appendToTerminal(newStuff) {
+      serialResultsDiv.innerHTML += newStuff;
+      if (serialResultsDiv.innerHTML.length > 3000) serialResultsDiv.innerHTML = serialResultsDiv.innerHTML.slice(serialResultsDiv.innerHTML.length - 3000);
+
+      //scroll down to bottom of div
+      serialResultsDiv.scrollTop = serialResultsDiv.scrollHeight;
+  }
+
+  document.getElementById("cmdLine").addEventListener("keyup", async function (event) {
+      if (event.keyCode === 13) {
+          sendSerialLine();
+      }
+  })
+
+  document.getElementById("baud").value = (localStorage.baud == undefined ? 9600 : localStorage.baud);
+  document.getElementById("addLine").checked = (localStorage.addLine == "false" ? false : true);
+  document.getElementById("echoOn").checked = (localStorage.echoOn == "false" ? false : true); 
+
